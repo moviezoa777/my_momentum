b5adebdd8d3db2103ad93ec92ca2ebb46200bbba
moviezoa777 <154862405+moviezoa777@users.noreply.github.com>
2024-08-11 16:37:18 +0900

    third commit
    deleteStorage( ) 함수 수정

---
+++ js/script-ex.js
@@ -0,0 +1,47 @@
+const movieForm = document.querySelector("#movie-form");
+const movieInput = document.querySelector("#movie-input");
+const movieButton = document.querySelector("#movie-button");
+const movieList = document.querySelector("#movie-list");
+let toWatchLists = [];
+let newObject = {};
+
+// deleteStorage()함수에서 이벤트가 발생한 imgTag의 부모 요소(li)를 찾아서 
+// 로컬 스토리지에서 제거하고, 화면에서도 제거하도록 한다.
+function deleteStorage(event){    
+    const li = event.target.parentElement;
+    const movieTitle = li.firstChild.textContent;
+
+    localStorage.removeItem(movieTitle);
+    li.remove();
+}
+
+function saveList(movieName){  //movieName매개변수 추가
+    newObject = {
+        title : movieName,
+        id : Date.now(),
+   }
+   localStorage.setItem(newObject.title , newObject.id );  
+   toWatchLists.push(newObject);
+}
+function makeNode(){
+    const movieName = movieInput.value; //movieName을 makeNode()함수에서 초기화
+    const listTag = document.createElement("li");
+    const textNode= document.createTextNode(movieName);
+    listTag.appendChild(textNode);
+    movieList.appendChild(listTag);
+
+    const imgTag = document.createElement("img");
+    const imgNode = document.createAttribute("src");
+    imgNode.value = "images/trashcan.png";
+    imgTag.setAttributeNode(imgNode);
+    listTag.appendChild(imgTag);
+    saveList(movieName); // movieName 인수 추가
+
+    imgTag.addEventListener("click", deleteStorage);
+    // imgTag에 이벤트 리스너를 추가하는 부분을 makeNode()함수에서 처리하도록 한다.
+}
+function summitList(event){
+    event.preventDefault();
+    makeNode();
+} 
+movieButton.addEventListener("click", summitList);
--- js/script.js
+++ js/script.js
@@ -2,14 +2,53 @@ const movieForm = document.querySelector("#movie-form");
 const movieInput = document.querySelector("#movie-input");
 const movieButton = document.querySelector("#movie-button");
 const movieList = document.querySelector("#movie-list");
+let movieName = movieInput.value;
+let toWatchs = [];
 
-function addList(){
-    const list = document.createElement("li");
-    const movieName = movieInput.value;
+
+function makeNode(){
+    const listTag = document.createElement("li");
+    movieName = movieInput.value;
+
     const textNode= document.createTextNode(movieName);
-    list.appendChild(textNode);
-    movieList.appendChild(list);
+    listTag.appendChild(textNode);
+    movieList.appendChild(listTag);
 
+    const imgTag = document.createElement("img");
+    const imgNode = document.createAttribute("src");
+    imgNode.value = "images/trashcan.png";
+    imgTag.setAttributeNode(imgNode);
+    listTag.appendChild(imgTag);
+
+    const newObject = {
+        title : movieName,
+        id : Date.now(),
+    }
+   localStorage.setItem(newObject.title , newObject.id );  
+   toWatchs.push(newObject);
+
+   imgTag.addEventListener("click", function(){
+    this.parentNode.remove(); 
+    })
 }
 
+
+function addList(event){
+    event.preventDefault();
+    makeNode();
+} 
+
 movieButton.addEventListener("click", addList);
+
+if(toWatchs !== null){
+    toWatchs.forEach(makeNode);
+}
+
+
+
+
+
+
+
+
+
